3a9f7c7bd9a50c51c0d266b8ac207b1f69e9af60
knightingal <yujianming1985@hotmail.com>
2024-04-26 22:32:00 +0800

    post method in axum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
-use axum::{routing::{get, post}, Router};
+use axum::{routing::{get, post}, Json, Router};
+use hyper::StatusCode;
+use serde_derive::{Deserialize, Serialize};
 
 mod test_main;
 mod test_aes;
@@ -8,7 +10,7 @@ mod test_aes;
 async fn main() {
   let app = Router::new()
     .route("/", get(root))
-    // .route("/users", post(create_user))
+    .route("/users", post(create_user))
     ;
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
   axum::serve(listener, app).await.unwrap();
@@ -16,4 +18,25 @@ async fn main() {
 
 async fn root() -> &'static str {
   "Hello World!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+  let user = User {
+    id: 1337,
+    username: payload.username
+  };
+
+  (StatusCode::CREATED, Json(user))
+}
+
+
+#[derive(Deserialize)]
+struct CreateUser {
+  username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+  id: u64,
+  username: String,
 }
