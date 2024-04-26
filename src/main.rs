b332e8950bf0c9fcf0ef8d15b432ad05e9d0c6f7
knightingal <yujianming1985@hotmail.com>
2024-04-26 22:41:11 +0800

    add path bind

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use axum::{routing::{get, post}, Json, Router};
+use axum::{extract::Path, routing::{get, post}, Json, Router};
 use hyper::StatusCode;
 use serde_derive::{Deserialize, Serialize};
 
@@ -10,7 +10,7 @@ mod test_aes;
 async fn main() {
   let app = Router::new()
     .route("/", get(root))
-    .route("/users", post(create_user))
+    .route("/users/name/:name/age/:age", post(create_user))
     ;
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
   axum::serve(listener, app).await.unwrap();
@@ -20,9 +20,13 @@ async fn root() -> &'static str {
   "Hello World!"
 }
 
-async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+async fn create_user(Path((name,age)): Path<(String, u32)>, Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+  let name:String = name;
+  let age: u32 = age;
+
   let user = User {
     id: 1337,
+    age, name,
     username: payload.username
   };
 
@@ -38,5 +42,7 @@ struct CreateUser {
 #[derive(Serialize)]
 struct User {
   id: u64,
+  age: u32,
+  name: String,
   username: String,
 }
