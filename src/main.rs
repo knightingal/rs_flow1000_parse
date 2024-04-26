a7a2974859cfe36b44d870c9c140029b99d7118b
knightingal <yujianming1985@hotmail.com>
2024-04-26 22:16:53 +0800

    get method in axum

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,19 @@
-use std::net::SocketAddr;
 
-use http_body_util::{combinators::BoxBody, BodyExt};
-use hyper::{body::Bytes, server::conn::http1, service::service_fn, Method, Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
+use axum::{routing::{get, post}, Router};
 
 mod test_main;
 mod test_aes;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-  println!("Hello, world!");
-  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-  let listener = TcpListener::bind(addr).await?;
-  loop {
-    let (stream, _) = listener.accept().await?;
-    let io = TokioIo::new(stream);
-    tokio::task::spawn(async move{
-      if let Err(err) = http1::Builder::new()
-        .serve_connection(io, service_fn(flow1000_server)).await {
-          println!("Error serving connection:{:?}", err);
-        }
-
-    });
-  }
+async fn main() {
+  let app = Router::new()
+    .route("/", get(root))
+    // .route("/users", post(create_user))
+    ;
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  axum::serve(listener, app).await.unwrap();
 }
 
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-  http_body_util::Full::new(chunk.into())
-      .map_err(|never| match never {})
-      .boxed()
-}
-
-
-
-async fn flow1000_server(req: Request<hyper::body::Incoming>) 
-  -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-  match (req.method(), req.uri().path()) {
-    (&Method::GET, "/") => Ok(Response::new(full("hello"))),
-    _ => Ok(Response::new(full("hello"))),
-      
-  }
+async fn root() -> &'static str {
+  "Hello World!"
 }
