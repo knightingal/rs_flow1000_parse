93406da6a53d406e925cfc1bbc703fffc1780719
knightingal <yujianming1985@hotmail.com>
2024-04-27 22:07:42 +0800

    add video info handler

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ async fn main() {
   let app = Router::new()
     .route("/", get(root))
     .route("/users/name/:name/age/:age", post(create_user))
+    .route("/video-info/:base_index/*sub_dir", get(video_info_handler))
     ;
   let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
   axum::serve(listener, app).await.unwrap();
@@ -33,6 +34,23 @@ async fn create_user(Path((name,age)): Path<(String, u32)>, Json(payload): Json<
   (StatusCode::CREATED, Json(user))
 }
 
+async fn video_info_handler(Path((base_index, sub_dir)): Path<(u32, String)>) -> (StatusCode, Json<DirInfo>) {
+  println!("{}", base_index);
+  println!("{}", sub_dir);
+  let vi = VideoInfo {
+    id: 1,
+    video_file_name: "vid.mp4".to_string(),
+    cover_file_name: "vid.jpg".to_string(),
+  };
+
+  let di = DirInfo {
+    id: base_index,
+    sub_dir,
+    video_list: vec![vi]
+  };
+  (StatusCode::OK, Json(di))
+}
+
 
 #[derive(Deserialize)]
 struct CreateUser {
@@ -45,4 +63,22 @@ struct User {
   age: u32,
   name: String,
   username: String,
+}
+
+#[derive(Serialize)]
+struct VideoInfo {
+  id: u32,
+  #[serde(alias = "videoFileName")]
+  video_file_name: String,
+  #[serde(alias = "coverFileName")]
+  cover_file_name: String,
+}
+
+#[derive(Serialize)]
+struct DirInfo {
+  id: u32,
+  #[serde(alias = "subDir")]
+  sub_dir: String,
+  #[serde(alias = "videoList")]
+  video_list: Vec<VideoInfo>
 }
