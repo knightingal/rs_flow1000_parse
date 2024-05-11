c0bcf67951e5cde728e4a46272387f38c9d0ae53
knightingal <yujianming1985@hotmail.com>
2024-05-11 18:55:06 +0800

    add test for parse_designation

--- src/test_main.rs
+++ src/test_main.rs
@@ -2,9 +2,62 @@
 mod tests {
     use std::{fs::File, io::Write};
 
+    use crate::parse_designation;
+
   #[test]
   fn it_works() {
     assert_eq!(2 + 2, 4);
+  }
+
+  #[test]
+  fn test_parse_designation1() {
+    let file_name = String::from("ipx-091");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_len, 3);
+    assert_eq!(designation.num_len, 3);
+    assert_eq!(designation.char_part, "ipx");
+    assert_eq!(designation.num_part, "091");
+  }
+
+  #[test]
+  fn test_parse_designation2() {
+    let file_name = String::from("ipx091");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_len, 3);
+    assert_eq!(designation.num_len, 3);
+    assert_eq!(designation.char_part, "ipx");
+    assert_eq!(designation.num_part, "091");
+    
+  }
+
+  #[test]
+  fn test_parse_designation3() {
+    let file_name = String::from("中文ipx091");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_len, 3);
+    assert_eq!(designation.num_len, 3);
+    assert_eq!(designation.char_part, "ipx");
+    assert_eq!(designation.num_part, "091");
+    
+  }
+  #[test]
+  fn test_parse_designation4() {
+    let file_name = String::from("中文@ipx091");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_len, 3);
+    assert_eq!(designation.num_len, 3);
+    assert_eq!(designation.char_part, "ipx");
+    assert_eq!(designation.num_part, "091");
+  }
+
+  #[test]
+  fn test_parse_designation5() {
+    let file_name = String::from("中文-ipx091");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_len, 3);
+    assert_eq!(designation.num_len, 3);
+    assert_eq!(designation.char_part, "ipx");
+    assert_eq!(designation.num_part, "091");
   }
 
   #[test]
