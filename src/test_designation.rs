1b96c80d5f8b39bd00996e36df7860d544329310
knightingal <yujianming1985@hotmail.com>
2024-05-14 21:10:14 +0800

    more cases

--- src/test_designation.rs
+++ src/test_designation.rs
@@ -101,4 +101,27 @@ mod tests {
     assert_eq!(designation.num_final.unwrap(), "345");
   }
 
+  #[test]
+  fn test_parse_designation11() {
+    let file_name = String::from("aavv39.xyz@IPX888C.mp4");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_final.unwrap(), "IPX");
+    assert_eq!(designation.num_final.unwrap(), "888");
+  }
+
+  #[test]
+  fn test_parse_designation12() {
+    let file_name = String::from("gc2048.com-中文字幕 ，一，些中文SSNI897【水印】.mp4");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_final.unwrap(), "SSNI");
+    assert_eq!(designation.num_final.unwrap(), "897");
+  }
+
+  #[test]
+  fn test_parse_designation13() {
+    let file_name = String::from("SSIS-656~nyap2p.com.mp4");
+    let designation = parse_designation(&file_name);
+    assert_eq!(designation.char_final.unwrap(), "SSIS");
+    assert_eq!(designation.num_final.unwrap(), "656");
+  }
 }
