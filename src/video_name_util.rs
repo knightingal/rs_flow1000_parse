98bade612c3d3d7248294c6168df5c0af6f21ae2
knightingal <yujianming1985@hotmail.com>
2024-07-22 17:43:16 +0800

    test `video_match_to_cover` and `only_one_matched`

--- src/video_name_util.rs
+++ src/video_name_util.rs
@@ -1,13 +1,14 @@
 use std::borrow::{Borrow, BorrowMut};
 
-fn video_match_to_cover(video_file_name: String, img_file_name_list: &Vec<String>) -> Result<VideoCover, ()> {
 
+fn video_match_to_cover(video_file_name: String, img_file_name_list: &Vec<String>) -> Result<VideoCover, ()> {
   let pure_name = parse_pure_name(&video_file_name);
   let size = pure_name.len();
   for i in 0..size {
-    for j in 0..i+1 {
-      let sub1:String = pure_name.split_at(j + size -i).0.into();
+    for j in 0..i + 1 {
+      let sub1:String = pure_name.split_at(j + size - i).0.into();
       let sub2:String = sub1.split_at(j).1.into();
+      println!("{}", sub2);
       let ret = only_one_matched(sub2, img_file_name_list);
 
       if ret.is_ok() {
@@ -19,20 +20,35 @@ fn video_match_to_cover(video_file_name: String, img_file_name_list: &Vec<String
 }
 
 fn only_one_matched(src: String, img_file_name_list: &Vec<String>) -> Result<String, bool> {
-  let mut filted = img_file_name_list.into_iter().filter(|img_file_name| {
-    img_file_name.contains(&src)
+  let filted = img_file_name_list.into_iter().filter(|img_file_name| {
+    parse_pure_name(img_file_name).contains(&src)
   });
-  if filted.borrow_mut().count() > 1 {
+  let mut matched_vec: Vec<&String> = Vec::new();
+  for filted_it in filted {
+    matched_vec.push(filted_it);
+  }
+  if matched_vec.len() != 1 {
     return Err(false);
   }
 
-  return Ok(filted.next().unwrap().clone());
-
+  return Ok(matched_vec[0].clone());
 }
 
 fn parse_pure_name(file_name: &String) -> String {
   return String::from(file_name.split('.').next().unwrap());
+}
 
+#[test]
+fn video_match_to_cover_test() {
+  let ret = video_match_to_cover("1234567890.mp4".to_string(), &vec!["1234567890.jpg".to_string(), "398434979.jpg".to_string()]).unwrap();
+  assert_eq!(ret.video_file_name, "1234567890.mp4".to_string());
+  assert_eq!(ret.cover_file_name, "1234567890.jpg".to_string());
+}
+
+#[test]
+fn only_one_matched_test() {
+  let matched = only_one_matched("1234567890".to_string(), &vec!["1234567890.jpg".to_string(), "398434979.jpg".to_string()]).unwrap();
+  assert_eq!(matched, "1234567890.jpg".to_string());
 }
 
 struct VideoCover {
