2a70230c58d516b7700d7f79b00696e567644e55
knightingal <yujianming1985@hotmail.com>
2024-07-23 21:52:28 +0800

    add `sub_string_matched` function to fix string match

--- src/video_name_util.rs
+++ src/video_name_util.rs
@@ -25,10 +25,7 @@ fn video_match_to_cover(video_file_name: &String, img_file_name_list: &Vec<&Stri
   let size = pure_name.chars().count();
   for i in 0..size {
     for j in 0..i + 1 {
-      let sub1:String = pure_name.split_at(j + size - i).0.into();
-      let sub2:String = sub1.split_at(j).1.into();
-      println!("{}", sub2);
-      let ret = only_one_matched(&sub2, img_file_name_list);
+      let ret = only_one_matched(&pure_name, img_file_name_list, j, j + size - i);
 
       if ret.is_ok() {
         return Ok(VideoCover { video_file_name: video_file_name.clone(), cover_file_name: ret.unwrap() })
@@ -38,9 +35,9 @@ fn video_match_to_cover(video_file_name: &String, img_file_name_list: &Vec<&Stri
   Err(())
 }
 
-fn only_one_matched(src: &String, img_file_name_list: &Vec<&String>) -> Result<String, bool> {
+fn only_one_matched(src: &String, img_file_name_list: &Vec<&String>, start: usize, end: usize) -> Result<String, bool> {
   let filted = img_file_name_list.into_iter().filter(|img_file_name| {
-    img_file_name.contains(src)
+    sub_string_matched(src, &img_file_name, start, end)
   });
   let mut matched_vec: Vec<&String> = Vec::new();
   for filted_it in filted {
@@ -53,6 +50,26 @@ fn only_one_matched(src: &String, img_file_name_list: &Vec<&String>) -> Result<S
   return Ok(matched_vec[0].clone());
 }
 
+fn sub_string_matched(src: &String, target: &String, start: usize, end: usize) -> bool {
+  let target_len = target.chars().count();
+
+  for i in 0..target_len {
+    let mut target_iter = target.chars().skip(i);
+    let mut src_iter = src.chars().skip(start);
+    let mut matched = true;
+    for _ in start..end {
+      if src_iter.next().unwrap() != target_iter.next().unwrap()  {
+        matched = false;
+        break;
+      }
+    }
+    if matched {
+      return true;
+    }
+  }
+  return false;
+}
+
 fn parse_pure_name(file_name: &String) -> String {
   return (file_name.strip_suffix(".mp4").unwrap()).to_string();
 
@@ -63,12 +80,38 @@ fn video_match_to_cover_test() {
   let ret = video_match_to_cover(&"1234567890.mp4".to_string(), &vec![&"1234567890.jpg".to_string(), &"398434979.jpg".to_string()]).unwrap();
   assert_eq!(ret.video_file_name, "1234567890.mp4".to_string());
   assert_eq!(ret.cover_file_name, "1234567890.jpg".to_string());
+
+  let ret = video_match_to_cover(&"23456789.mp4".to_string(), &vec![&"1234567890.jpg".to_string(), &"398434979.jpg".to_string()]).unwrap();
+  assert_eq!(ret.video_file_name, "23456789.mp4".to_string());
+  assert_eq!(ret.cover_file_name, "1234567890.jpg".to_string());
+
+  let ret = video_match_to_cover(&"234567891.mp4".to_string(), &vec![&"1234567890.jpg".to_string(), &"398434979.jpg".to_string()]).unwrap();
+  assert_eq!(ret.video_file_name, "234567891.mp4".to_string());
+  assert_eq!(ret.cover_file_name, "1234567890.jpg".to_string());
+
+  let ret = video_match_to_cover(&"234567891.mp4".to_string(), &vec![&"1234567890.jpg".to_string(), &"1234567892.jpg".to_string(), &"398434979.jpg".to_string()]);
+  assert_eq!(ret.is_err(), true);
 }
 
 #[test]
 fn only_one_matched_test() {
-  let matched = only_one_matched(&"1234567890".to_string(), &vec![&"1234567890.jpg".to_string(), &"398434979.jpg".to_string()]).unwrap();
-  assert_eq!(matched, "1234567890.jpg".to_string());
+  // let matched = only_one_matched(&"1234567890".to_string(), &vec![&"1234567890.jpg".to_string(), &"398434979.jpg".to_string()]).unwrap();
+  // assert_eq!(matched, "1234567890.jpg".to_string());
+}
+
+#[test]
+fn test_sub_string_matched() {
+  let matched = sub_string_matched(&String::from("中国江西南京"), &String::from("江苏南京鼓楼"), 2, 4);
+  assert_eq!(matched, false);
+
+  let matched = sub_string_matched(&String::from("中国江苏南京"), &String::from("江苏南京鼓楼"), 2, 4);
+  assert_eq!(matched, true);
+
+  let matched = sub_string_matched(&String::from("中国江苏南京"), &String::from("江苏南京鼓楼"), 2, 6);
+  assert_eq!(matched, true);
+
+  let matched = sub_string_matched(&String::from("中国江苏南京"), &String::from("江苏南京鼓楼"), 1, 6);
+  assert_eq!(matched, false);
 }
 
 #[derive(Debug)]
