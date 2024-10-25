8e645df7cbfbd0356b91a377fb7c55bc586f8325
knightingal <yujianming1985@hotmail.com>
2024-10-25 22:57:33 +0800

    add `mock_stream_hander`

---
+++ src/stream_handlers.rs
@@ -0,0 +1,36 @@
+use axum::{body::{Body, Bytes}, response::Response, Error};
+use hyper::StatusCode;
+
+pub async fn mock_stream_hander() -> Response {
+
+  let response_builder = Response::builder().status(StatusCode::OK);
+  let mock_stream = MockStream::new();
+
+  response_builder
+        .body(Body::from_stream(mock_stream))
+        .unwrap()
+}
+
+struct MockStream {
+  done: bool
+}
+
+impl MockStream {
+  fn new() -> Self {
+    MockStream { done: false }
+  }
+}  
+
+impl futures_core::Stream for MockStream {
+    type Item = Result<Bytes, Error>;
+    fn poll_next(mut self: std::pin::Pin<&mut Self>, _: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
+      if self.done {
+        return std::task::Poll::Ready(None);
+      } else {
+        self.done = true;
+        let buf = Bytes::from_static(b"hello");
+        return std::task::Poll::Ready(Some(Ok(buf)));
+          
+      }
+    }
+}
