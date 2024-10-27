33765b1132e3383ad48f0d19167d836544781474
knightingal <yujianming1985@hotmail.com>
2024-10-27 11:15:21 +0800

    add `video_stream_hander` fn

--- src/stream_handlers.rs
+++ src/stream_handlers.rs
@@ -5,7 +5,7 @@ use axum::{
   response::Response,
   Error,
 };
-use hyper::StatusCode;
+use hyper::{HeaderMap, StatusCode};
 
 pub async fn mock_stream_hander() -> Response {
   let response_builder = Response::builder().status(StatusCode::OK);
@@ -20,6 +20,19 @@ pub async fn file_stream_hander() -> Response {
   let response_builder = Response::builder().status(StatusCode::OK);
   let mock_stream = FileStream::new();
 
+  response_builder
+    .body(Body::from_stream(mock_stream))
+    .unwrap()
+}
+
+pub async fn video_stream_hander() -> Response {
+  let mut response_builder = Response::builder().status(StatusCode::OK);
+  let mock_stream = VideoStream::new();
+
+  let mut header = HeaderMap::new();
+  header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
+  header.insert("content-type", "video/mp4".parse().unwrap());
+  *response_builder.headers_mut().unwrap() = header;
   response_builder
     .body(Body::from_stream(mock_stream))
     .unwrap()
@@ -63,6 +76,37 @@ impl FileStream {
 }
 
 impl futures_core::Stream for FileStream {
+  type Item = Result<Bytes, Error>;
+
+  fn poll_next(
+    mut self: std::pin::Pin<&mut Self>,
+    _: &mut std::task::Context<'_>,
+  ) -> std::task::Poll<Option<Self::Item>> {
+    let mut buf = [0u8; 1024];
+    let read_result = self.file.read(&mut buf);
+    match read_result {
+      Ok(read_len) => match read_len > 0 {
+        true => std::task::Poll::Ready(Some(Ok(Bytes::copy_from_slice(&buf).slice(0..read_len)))),
+        false => std::task::Poll::Ready(None),
+      },
+      Err(_) => std::task::Poll::Ready(None),
+    }
+  }
+}
+
+
+struct VideoStream {
+  file: File,
+}
+
+impl VideoStream {
+  fn new() -> Self {
+    let file = File::open("video-path.mp4").unwrap();
+    Self { file }
+  }
+}
+
+impl futures_core::Stream for VideoStream {
   type Item = Result<Bytes, Error>;
 
   fn poll_next(
