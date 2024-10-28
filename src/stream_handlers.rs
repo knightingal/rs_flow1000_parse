4cbb4608371939a0805ae011e43111e4acb7be7c
knightingal <yujianming1985@hotmail.com>
2024-10-28 21:22:37 +0800

    support `PARTIAL_CONTENT` response in `video_stream_hander`

--- src/stream_handlers.rs
+++ src/stream_handlers.rs
@@ -1,14 +1,12 @@
 //! Interface for stream.
 //!
 //! This module contains basic code to investigate and validate stream response based axum 
-use std::{env, fs::File, io::Read};
+use std::{env, fs::File, io::{Read, Seek}};
 
 use axum::{
-  body::{Body, Bytes},
-  response::Response,
-  Error,
+  body::{Body, Bytes}, extract::Request, response::Response, Error
 };
-use hyper::{HeaderMap, StatusCode};
+use hyper::{header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_LENGTH, CONTENT_RANGE, CONTENT_TYPE, RANGE}, HeaderMap, StatusCode};
 
 pub async fn mock_stream_hander() -> Response {
   let response_builder = Response::builder().status(StatusCode::OK);
@@ -28,13 +26,43 @@ pub async fn file_stream_hander() -> Response {
     .unwrap()
 }
 
-pub async fn video_stream_hander() -> Response {
-  let mut response_builder = Response::builder().status(StatusCode::OK);
-  let mock_stream = VideoStream::new();
+pub async fn video_stream_hander(req: Request) -> Response {
+
+  let db_path_env = env::var("DEMO_VIDEO").unwrap();
+  let path = std::path::Path::new(&db_path_env);
+  let file_size = path.metadata().map_or_else(|_| {0}, |m|{m.len()});
+  // let file_size = 100000u64;
+  let range_header = req.headers().get(RANGE);
+  
+  let (start, end, content_length, part) = 
+  match range_header {
+    Some(range_header) => {
+      let (_, value) = range_header.to_str().unwrap().split_once("=").unwrap();
+
+      let (start, end) = value.split_once("-").unwrap();
+      let start:u64 = start.parse().unwrap();
+      let end:u64 = end.parse().unwrap_or(file_size - 1);
+      (start, end, file_size - start, true)
+    }
+    _ => {
+      (0, file_size - 1, file_size, false)
+    }
+  };
+
+  let status_code = match part {
+      true => StatusCode::PARTIAL_CONTENT,
+      false => StatusCode::OK,
+    };
+  let mut response_builder = Response::builder().status(status_code);
+  let mock_stream = VideoStream::new(start);
 
   let mut header = HeaderMap::new();
-  header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
-  header.insert("content-type", "video/mp4".parse().unwrap());
+  header.insert(ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
+  header.insert(CONTENT_TYPE, "video/mp4".parse().unwrap());
+  header.insert(CONTENT_LENGTH, content_length.into());
+  if part {
+    header.insert(CONTENT_RANGE, format!("bytes {}-{}/{}", start, end, file_size).parse().unwrap());
+  }
   *response_builder.headers_mut().unwrap() = header;
   response_builder
     .body(Body::from_stream(mock_stream))
@@ -103,10 +131,11 @@ struct VideoStream {
 }
 
 impl VideoStream {
-  fn new() -> Self {
+  fn new(start: u64) -> Self {
 
     let db_path_env = env::var("DEMO_VIDEO").unwrap();
-    let file = File::open(db_path_env).unwrap();
+    let mut file = File::open(db_path_env).unwrap();
+    let _ = file.seek(std::io::SeekFrom::Start(start));
     Self { file }
   }
 }
