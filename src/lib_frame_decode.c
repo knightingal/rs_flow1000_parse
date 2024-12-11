5b453197267dab69267b545336304a6343307a71
knightingal <yujianming1985@hotmail.com>
2024-12-11 23:10:27 +0800

    fix ffmpeg warning

--- src/lib_frame_decode.c
+++ src/lib_frame_decode.c
@@ -81,6 +81,9 @@ static int frame_array_to_image(AVFrame **frame_array, enum AVCodecID code_id, u
   struct SwsContext *sws_context = NULL;
   codec = avcodec_find_encoder(code_id);
   ctx = avcodec_alloc_context3(codec);
+
+  const enum AVPixelFormat *pix_fmts;
+  avcodec_get_supported_config(NULL, codec, AV_CODEC_CONFIG_PIX_FORMAT, 0, (const void **) &pix_fmts, NULL);
   int dest_width = frame_array[0]->width;
   int dest_height = frame_array[0]->height;
   ctx->width = frame_array[0]->width;
@@ -90,7 +93,7 @@ static int frame_array_to_image(AVFrame **frame_array, enum AVCodecID code_id, u
   ctx->time_base.den = 25;
   ctx->gop_size = 10;
   ctx->max_b_frames = 0;
-  ctx->pix_fmt = *codec->pix_fmts;
+  ctx->pix_fmt = *pix_fmts;
   ret = avcodec_open2(ctx, codec, NULL);
   rgb_frame = frame_to_rgb_buff(frame_array[0], 0, ctx, NULL);
   rgb_frame->format = ctx->pix_fmt;
@@ -136,7 +139,9 @@ static int frame_to_image(AVFrame *frame, enum AVCodecID code_id, uint8_t *outbu
     printf("codec non found\n");
     goto error;
   }
-  if (!codec->pix_fmts)
+  const enum AVPixelFormat *pix_fmts;
+  ret = avcodec_get_supported_config(NULL, codec, AV_CODEC_CONFIG_PIX_FORMAT, 0, (const void **) &pix_fmts, NULL);
+  if (ret < 0)
   {
     ret = -1;
     printf("codec non support pix_fmt\n");
@@ -154,7 +159,7 @@ static int frame_to_image(AVFrame *frame, enum AVCodecID code_id, uint8_t *outbu
   ctx->time_base.den = 25;
   ctx->gop_size = 10;
   ctx->max_b_frames = 0;
-  ctx->pix_fmt = *codec->pix_fmts;
+  ctx->pix_fmt = *pix_fmts;
   ret = avcodec_open2(ctx, codec, NULL);
   if (ret < 0)
   {
