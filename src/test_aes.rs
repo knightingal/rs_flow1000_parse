0e23516222b3071dc6c2c8b2727bcdb9c7121432
knightingal <yujianming1985@hotmail.com>
2025-09-13 21:41:43 +0800

    add ut to test `cfb_file_streaming_v2`

--- src/test_aes.rs
+++ src/test_aes.rs
@@ -66,6 +66,26 @@ extern "C" {
     }
   }
 
+  #[test]
+  fn cfb_extern_test3() {
+    unsafe {
+      if cfg!(reallink) {
+        let key = "passwordpasswordpasswordpassword";
+        let iv = "2021000120210001";
+        let mut w: [u32; 60] = [0; 60];
+        key_expansion(key.as_ptr(), w.as_mut_ptr());
+        println!("key_expansion: {:?}", w);
+        let ret = cfb_file_streaming_v2(
+          w.as_ptr(),
+          iv.as_ptr(),
+          "/home/knightingal/demo_video.mp4\0".as_ptr() as *const c_char,
+          "/home/knightingal/rust_cfb.mp4.bin\0".as_ptr() as *const c_char,
+        );
+        println!("cfb_file_streaming_v2 ret: {}", ret);
+      }
+    }
+  }
+
 
   #[test]
   fn ctr_test() {
