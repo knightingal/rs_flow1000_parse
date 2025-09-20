c4d68a76859d516acd478e2f0941741e9aca9d6e
knightingal <yujianming1985@hotmail.com>
2025-09-20 16:09:26 +0800

    add `cfb_video_by_id`

--- src/handles.rs
+++ src/handles.rs
@@ -959,6 +959,84 @@ pub fn parse_and_update_meta_info_by_id(id: u32, video_file_name: String, cover_
   });
 }
 
+pub async fn cfb_video_by_id(
+  Path(id): Path<u32>,
+) -> (StatusCode, Json<Vec<String>>) {
+
+  tracing::debug!("cfb_video_by_id handler: id: {}", id);
+
+  let mount_configs = query_mount_configs();
+
+  let sqlite_conn = get_sqlite_connection();
+  let mut sql = String::from("select vi.id, ");
+  let dir_path_name: &str;
+  unsafe {
+    dir_path_name = if *IS_LINUX.unwrap() {
+      "dir_path"
+    } else {
+      "win_dir_path"
+    }
+  }
+  sql += "mbd.";
+  sql += dir_path_name;
+  sql += " , vi.video_file_name, vi.base_index, mbd.url_prefix, mbd.api_version 
+    from video_info vi left join mp4_base_dir mbd on vi.base_index = mbd.id where vi.id = :id";
+  let video_entity = sqlite_conn
+    .query_row(sql.as_str(), named_params! {":id": id}, |row| {
+      Ok(VideoEntity::new_by_file_name(
+        row.get_unwrap("id"),
+        row.get_unwrap("video_file_name"),
+        String::new(),
+        row.get_unwrap(dir_path_name),
+        row.get_unwrap("base_index"),
+      ))
+    })
+    .unwrap();
+  let file_path = video_entity.dir_path + "/" + video_entity.video_file_name.as_str();
+
+  let mut target_dir = mount_configs[0].dir_path.clone();
+  target_dir.push_str("/cfb/");
+  let mut buffer = itoa::Buffer::new();
+  let base_index_str = buffer.format(video_entity.base_index);
+  target_dir.push_str(base_index_str);
+  let last_index = file_path.rfind('/').unwrap();
+  let (parenet_dir, file_name) = file_path.split_at(last_index);
+  target_dir.push_str(parenet_dir);
+
+  let target_dir_path = std::path::Path::new(&target_dir);
+  if !target_dir_path.exists() {
+    DirBuilder::new()
+      .recursive(true)
+      .create(target_dir_path).unwrap();
+  }
+
+  let mut target_file_path = target_dir;
+  target_file_path.push_str(file_name);
+  target_file_path.push_str(".bin");
+
+  // let file_name = CString::new(file_name).unwrap();
+  let input_file_path = CString::new(file_path.as_str()).unwrap();
+
+  let iv = "2021000120210001";
+
+  let move_target_file_path = CString::new(target_file_path.as_str()).unwrap();
+
+  // thread::spawn(move || {
+  //   unsafe {
+  //     cfb_file_streaming_v2(
+  //         0 as *const u32,
+  //         iv.as_ptr(),
+  //         input_file_path.as_ptr() as *const c_char,
+  //         move_target_file_path.as_ptr() as *const c_char
+  //     );
+  //   }
+  // });
+
+  let resp_vec = vec![target_file_path, file_path];
+
+  (StatusCode::OK, Json(resp_vec))
+}
+
 pub async fn cfb_video_by_path(
   Path((base_index, sub_dir)): Path<(u32, String)>,
 ) -> (StatusCode, Json<Vec<String>>) {
