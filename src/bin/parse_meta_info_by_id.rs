cd04b8e3bcb8a4b4c198ab1089d1abe8825b722e
knightingal <yujianming1985@hotmail.com>
2025-12-07 19:58:49 +0800

    add parse_meta_info_by_id bin

---
+++ src/bin/parse_meta_info_by_id.rs
@@ -0,0 +1,13 @@
+use rs_flow1000_parse::base_lib::{parse_and_update_meta_info_by_id, video_file_path_by_id};
+
+fn main() {
+  let id = 1;
+  let file_names = video_file_path_by_id(id);
+  println!("file_names:{:?}", file_names);
+
+  file_names
+    .into_iter()
+    .for_each(|(id, video_file_name, cover_file_name)| {
+      parse_and_update_meta_info_by_id(id, video_file_name, cover_file_name);
+    });
+}
