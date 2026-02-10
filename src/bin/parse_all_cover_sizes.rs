1f24d5942ee30f41eb73e08f7442e5fe10a6c5a4
knightingal <yujianming1985@hotmail.com>
2026-02-10 21:27:34 +0800

    fix parse_all_cover_size

--- src/bin/parse_all_cover_sizes.rs
+++ src/bin/parse_all_cover_sizes.rs
@@ -4,18 +4,25 @@ fn main() {
 
   linux_init();
 
-  scan_all_by_id(|id| {
+  let size_list = scan_all_by_id(|id| {
     println!("id: {}", id);
     let res = parse_image_size_by_id(id);
     if res.is_ok() {
       let (width, height) = res.unwrap();
-      println!("width:{}, heigth:{}", width, height);
+      return Ok((id, width, height));
+    } else {
+      return Err(())
+    }
+  });
+
+  size_list.iter().for_each(|res| {
+    if res.is_ok() {
+      let (id, width, height) = res.unwrap();
+      println!("id{}, width:{}, heigth:{}",id, width, height);
       let update_ret = update_cover_size_by_id(id, width, height);
       if update_ret.is_err() {
         println!("fail to update id:{}, error:{}", id, update_ret.unwrap_err())
       }
-    } else {
-      println!("parse {} failed", id);
     }
   });
 }
