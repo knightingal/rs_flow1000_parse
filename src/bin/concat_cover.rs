94634d6dc1d3079c38dafb9395625d796e511eba
knightingal <yujianming1985@hotmail.com>
2026-05-11 23:16:18 +0800

    add args support in concat cover

--- src/bin/concat_cover.rs
+++ src/bin/concat_cover.rs
@@ -1,11 +1,22 @@
 use rs_flow1000_parse::{base_lib::{concat_cover, os_init,} };
+use std::env;
 
 
 fn main() {
   println!("concat cover!");
 
+  let args: Vec<String> = env::args().collect();
+  if args.len() < 2 {
+    println!("invalid arg. input arg to indicate path such as \"/202512\"");
+    return;
+  }
+
+  let sub_dir: String = args[1].clone();
+
+  println!("sub_dir {}", sub_dir);
+
   os_init();
-  let dir_name = String::from("/202604");
+  let dir_name = sub_dir;
   concat_cover(dir_name);
 
 
