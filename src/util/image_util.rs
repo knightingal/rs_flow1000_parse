d474daaa2f52a58401a5fe5d6e4308cd12f4149d
knightingal <yujianming1985@hotmail.com>
2026-07-09 21:26:13 +0800

    add parse wepb funtcion

--- src/util/image_util.rs
+++ src/util/image_util.rs
@@ -84,7 +84,68 @@ pub fn parse_jpg_size(mut jpg: File, start: u64) -> io::Result<(u32, u32)> {
 pub fn parse_webp_size(mut png: File, start: u64) -> io::Result<(u32, u32)> {
   png.seek(std::io::SeekFrom::Start(start))?;
 
-  Ok((0, 0))
+  let mut buf = [0u8; 16];
+  png.read_exact(&mut buf)?;
+  
+  let riff = String::from_utf8(Vec::from(&buf[0..4])).map_err(|_| Error::new(ErrorKind::Other, "unexpected header"))?;
+  if riff != "RIFF" {
+    return Err(
+      Error::new(ErrorKind::Other, "unexpected header")
+    );
+  }
+  
+  let type_ = String::from_utf8(Vec::from(&buf[8..12])).map_err(|_| Error::new(ErrorKind::Other, "unexpected type"))?;
+  if type_ != "WEBP" {
+    return Err(
+      Error::new(ErrorKind::Other, "unexpected type")
+    );
+  }
+  
+  let vp8 = String::from_utf8(Vec::from(&buf[12..16])).map_err(|_| Error::new(ErrorKind::Other, "unexpected vp8"))?;
+  if vp8 != "VP8 " && vp8 != "VP8X" {
+    return Err(
+      Error::new(ErrorKind::Other, "unexpected vp8")
+    );
+  }
+  
+  if vp8 == "VP8 " {
+    let mut buf = [0u8; 4];
+    png.read_exact(&mut buf)?;
+    
+    let mut buf = [0u8; 16];
+    let read_len = png.read(&mut buf)?;
+    if read_len != 16 {
+      return Err(Error::new(ErrorKind::Other, "read file failed, read header len: " + read_len.to_string())));
+    }
+    
+    let data6 = buf[6] as u32;
+    let data7 = buf[7] as u32;
+    let data8 = buf[8] as u32;
+    let data9 = buf[9] as u32;
+    
+    let w = ((data7 << 8) | data6) & 0x3fff;
+    let h = ((data9 << 8) | data8) & 0x3fff;
+    
+    Ok((h, w))
+  } else {
+    let mut buf = [0u8; 20];
+    let read_len = png.read(&mut buf)?;
+    if read_len != 20 {
+      return Err(Error::new(ErrorKind::Other, "read file failed, read header len: " + read_len.to_string())));
+    }
+    
+    let data12 = buf[8] as u32;
+    let data13 = buf[9] as u32;
+    let data14 = buf[10] as u32;
+    let data15 = buf[11] as u32;
+    let data16 = buf[12] as u32;
+    let data17 = buf[13] as u32;
+    
+    let width = 1 + (data14 << 16 | data13 << 8 | data12);
+    let height = 1 + (data17 << 16 | data16 << 8 | data15);
+    
+    Ok((height, width))
+  }
 }
 
 pub fn parse_png_size(mut png: File, start: u64) -> io::Result<(u32, u32)> {
