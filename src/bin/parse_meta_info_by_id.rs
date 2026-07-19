e9767d6057cb533a5a17b34adeeba61ec5892107
knightingal <yujianming1985@hotmail.com>
2026-07-19 23:16:54 +0800

    parse param of parse_meta_info_by_id

--- src/bin/parse_meta_info_by_id.rs
+++ src/bin/parse_meta_info_by_id.rs
@@ -1,10 +1,18 @@
+use std::env;
+
 use rs_flow1000_parse::base_lib::{os_init, parse_and_update_meta_info_by_id, video_file_path_by_id};
 
 fn main() {
 
   tracing_subscriber::fmt::init();
+
+  let args: Vec<String> = env::args().collect();
+  if args.len() < 2 {
+    tracing::error!("invalid args. input args to indicate video id such as \"3\"");
+    return;
+  }
   os_init();
-  let id = 1;
+  let id = args[1].parse().unwrap();
   let file_names = video_file_path_by_id(id);
   tracing::info!("file_names:{:?}", file_names);
 
