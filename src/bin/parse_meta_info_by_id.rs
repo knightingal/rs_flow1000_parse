9ddffb5ebd2cb29394631eb3352c50b15cb1c8eb
knightingal <yujianming1985@hotmail.com>
2026-07-22 20:18:15 +0800

    consume `refresh_video_and_cover_by_id` function in parse_meta_info_by_id bin

--- src/bin/parse_meta_info_by_id.rs
+++ src/bin/parse_meta_info_by_id.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use rs_flow1000_parse::base_lib::{os_init, parse_and_update_meta_info_by_id, video_file_path_by_id};
+use rs_flow1000_parse::base_lib::{os_init, refresh_video_and_cover_by_id};
 
 fn main() {
 
@@ -13,12 +13,5 @@ fn main() {
   }
   os_init();
   let id = args[1].parse().unwrap();
-  let file_names = video_file_path_by_id(id);
-  tracing::info!("file_names:{:?}", file_names);
-
-  file_names
-    .into_iter()
-    .for_each(|(id, video_file_name, cover_file_name, _)| {
-      parse_and_update_meta_info_by_id(id, video_file_name, cover_file_name);
-    });
+  refresh_video_and_cover_by_id(id);
 }
