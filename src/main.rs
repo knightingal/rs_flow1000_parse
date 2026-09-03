d347255436d4097ccc20d5b8f30f9eed4815320c
knightingal <yujianming1985@hotmail.com>
2026-09-03 21:11:15 +0800

    filter log level by env param

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,13 @@ use serde_derive::{Deserialize, Serialize};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::{Layer, filter::filter_fn, layer::SubscriberExt, util::SubscriberInitExt};
 use std::{
-  env,
-  ffi::{c_char, c_void, CStr, CString},
-  future::Future,
-  time::Duration,
+  env, ffi::{CStr, CString, c_char, c_void}, future::Future, str::FromStr, time::Duration,
 };
 use stream_handlers::{
   file_stream_handler, image_stream_by_path_handler, mock_stream_handler, video_exist_handler, video_stream_handler,
 };
 use tower_http::trace::TraceLayer;
-use tracing::{Span, level_filters::LevelFilter};
+use tracing::{Level, Span, level_filters::LevelFilter};
 
 use sysinfo::System;
 
@@ -129,6 +126,9 @@ extern "C" {
 #[tokio::main]
 async fn main() {
   // env_logger::init();
+
+  let rust_log_env = env::var("RUST_LOG")
+    .unwrap_or_else(|_| String::from("INFO"));
   let sql_appender = RollingFileAppender::new(Rotation::NEVER, "./", "sql.log");
   let (sql_blocking, _guard) = tracing_appender::non_blocking(sql_appender);
   let sql_layer = tracing_subscriber::fmt::layer()
@@ -140,7 +140,11 @@ async fn main() {
   let (std_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
   let std_layer = tracing_subscriber::fmt::layer()
     .with_writer(std_blocking)
-    .with_filter(LevelFilter::INFO);
+    .with_filter(
+      LevelFilter::from_level(
+        Level::from_str(&rust_log_env).unwrap()
+      )
+    );
 
   tracing_subscriber::registry()
     .with(sql_layer)
