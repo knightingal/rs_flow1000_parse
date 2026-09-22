f111237a7d6e9258da209d10b9359c8dfbdcc9db
knightingal <yujianming1985@hotmail.com>
2026-09-22 21:43:57 +0800

    add NoneSqlLevelFilter

--- src/log_util.rs
+++ src/log_util.rs
@@ -1,6 +1,8 @@
 use std::{env, fmt, str::FromStr};
-use tracing::{level_filters::LevelFilter, Event, Level, Subscriber};
+use tracing::Metadata;
+use tracing::{Event, Level, Subscriber};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::layer::{Context, Filter};
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::{
   filter::filter_fn,
@@ -112,6 +114,32 @@ pub struct LogGuards {
   _std_guard: tracing_appender::non_blocking::WorkerGuard,
 }
 
+struct NoneSqlLevelFilter {
+  level: Level
+}
+
+impl<S> Filter<S> for NoneSqlLevelFilter {
+    #[doc = r" Returns `true` if this layer is interested in a span or event with the"]
+    #[doc = r" given [`Metadata`] in the current [`Context`], similarly to"]
+    #[doc = r" [`Subscriber::enabled`]."]
+    #[doc = r""]
+    #[doc = r" If this returns `false`, the span or event will be disabled _for the"]
+    #[doc = r" wrapped [`Layer`]_. Unlike [`Layer::enabled`], the span or event will"]
+    #[doc = r" still be recorded if any _other_ layers choose to enable it. However,"]
+    #[doc = r" the layer [filtered] by this filter will skip recording that span or"]
+    #[doc = r" event."]
+    #[doc = r""]
+    #[doc = r" If all layers indicate that they do not wish to see this span or event,"]
+    #[doc = r" it will be disabled."]
+    #[doc = r""]
+    #[doc = r" [`metadata`]: tracing_core::Metadata"]
+    #[doc = r" [`Subscriber::enabled`]: tracing_core::Subscriber::enabled"]
+    #[doc = r" [filtered]: crate::filter::Filtered"]
+    fn enabled(&self, meta: &Metadata<'_>, _cx: &Context<'_, S>) -> bool {
+      return meta.level() <= &self.level && meta.target() != "sql";
+    }
+}
+
 pub fn log_init() -> LogGuards {
   let sql_appender = RollingFileAppender::new(Rotation::NEVER, "./", "sql.log");
   let (sql_blocking, sql_guard) = tracing_appender::non_blocking(sql_appender);
@@ -120,15 +148,13 @@ pub fn log_init() -> LogGuards {
     .with_writer(sql_blocking)
     .with_filter(filter_fn(|metadata| metadata.target() == "sql"));
 
+  let rust_log_env = env::var("RUST_LOG").unwrap_or_else(|_| String::from("INFO"));
+  let log_level = Level::from_str(&rust_log_env).unwrap();
 
   let (std_blocking, std_guard) = tracing_appender::non_blocking(std::io::stdout());
   let std_layer = tracing_subscriber::fmt::layer()
     .with_writer(std_blocking)
-    .with_filter(filter_fn(|metadata| { 
-      let rust_log_env = env::var("RUST_LOG").unwrap_or_else(|_| String::from("INFO"));
-      let log_level = Level::from_str(&rust_log_env).unwrap();
-      return metadata.target() != "sql" && metadata.level() >= &log_level; 
-    }));
+    .with_filter(NoneSqlLevelFilter{level: log_level});
 
   tracing_subscriber::registry()
     .with(sql_layer)
